3ceecf9b1dc4c7a9236adde002b17668142ad3e3
kd72004 <109151753+kd72004@users.noreply.github.com>
2022-07-23 17:28:50 +0530

    Add files via upload

---
+++ problem-7.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+     for (int i = (n * 2); i >= 1; --i)
+    {
+        if (i % 2 == 0)
+            cout << i << endl;
+    }
+}   
